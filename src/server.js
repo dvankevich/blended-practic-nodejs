160b99ceeffc5308b05e8b37482c57b7b6a5ff5d
DenVik <denyagarkusha2@gmail.com>
2025-01-19 10:48:30 +0100

    task 4 have done

--- src/server.js
+++ src/server.js
@@ -4,12 +4,15 @@ import cors from 'cors';
 import { env } from './utils/env.js';
 import { errorHandler } from './middlewares/errorHandler.js';
 import { notFoundHandler } from './middlewares/notFoundHandler.js';
-import { getAllProducts } from './services/products.js';
+import { getAllProducts, getProductsById } from './services/products.js';
 
 const PORT = Number(env('PORT', '3000'));
 
 export const setupServer = () => {
   const app = express();
+
+  app.use(express.json());
+  app.use(cors());
 
   app.get('/products', async (req, res) => {
     const products = await getAllProducts();
@@ -21,8 +24,25 @@ export const setupServer = () => {
     });
   });
 
-  app.use(express.json());
-  app.use(cors());
+  app.get('/products/:id', async (req, res, next) => {
+
+    const { id } = req.params;
+
+    const data = await getProductsById(id);
+
+    if (!data) {
+      res.status(404).json({
+        status: 404,
+        message: 'Products is not found'
+      });
+    }
+
+    res.status(200).json({
+      status: 200,
+      message: `Successfully found product with id ${id}!`,
+      data: data,
+    });
+  });
 
   //   app.use(productsRouter);
 
