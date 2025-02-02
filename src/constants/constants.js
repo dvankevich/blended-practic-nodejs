a92414a1caa617e86b11758960c582c9d0573799
Falconoff <sokolovserega1982@gmail.com>
2025-02-02 13:02:52 +0200

    add: sort

--- src/constants/constants.js
+++ src/constants/constants.js
@@ -2,3 +2,5 @@ export const SORT_ORDER = {
   ASC: 'asc',
   DESC: 'desc',
 };
+
+export const CATEGORY_LIST = ['books', 'electronics', 'clothing', 'other'];
--- src/validation/validationProductsSchema.js
+++ src/validation/validationProductsSchema.js
@@ -1,15 +1,18 @@
-import Joi from "joi";
+import Joi from 'joi';
+import { CATEGORY_LIST } from '../constants/constants.js';
 
 export const validationProductsSchema = Joi.object({
-    name: Joi.string().required(),
-    price: Joi.number().required(),
-    category: Joi.string().required().valid('books', 'electronics', 'clothing', 'other'),
-    description: Joi.string(),
+  name: Joi.string().required(),
+  price: Joi.number().required(),
+  category: Joi.string()
+    .required()
+    .valid(...CATEGORY_LIST),
+  description: Joi.string(),
 });
 
 export const patchValidationProductsSchema = Joi.object({
-    name: Joi.string(),
-    price: Joi.number(),
-    category: Joi.string().valid('books', 'electronics', 'clothing', 'other'),
-    description: Joi.string(),
+  name: Joi.string(),
+  price: Joi.number(),
+  category: Joi.string().valid(...CATEGORY_LIST),
+  description: Joi.string(),
 });
