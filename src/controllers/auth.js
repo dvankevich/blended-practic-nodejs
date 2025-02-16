663f1d21cb7bfbdf16df245a0d47112194e1a9b5
Makyym <168359394+Makyym@users.noreply.github.com>
2025-02-16 11:18:25 +0200

    something went wrong

--- src/controllers/auth.js
+++ src/controllers/auth.js
@@ -1,11 +1,32 @@
-import { registerUser } from '../services/auth.js';
+import { ONE_DAY } from '../constants/constants.js';
+import { loginUser, registerUser } from '../services/auth.js';
 
 export const registerUserController = async (req, res) => {
-  const user = await registerUser(req.body);
+  console.log("test");
 
+  const user = await registerUser(req.body);
   res.status(201).json({
     status: 201,
     message: 'Successfully registered a user!',
     data: user,
+  });
+};
+
+export const loginUserController = async (req, res) => {
+  const session = await loginUser(req.body);
+  res.cookie('refreshToken', session.refreshToken, {
+    httpOnly: true,
+    expires: new Date(Date.now() + ONE_DAY),
+  });
+  res.cookie('sessionId', session._id, {
+    httpOnly: true,
+    expires: new Date(Date.now() + ONE_DAY),
+  });
+  res.json({
+    status: 200,
+    message: 'Successfully logged in',
+    data: {
+      accessToken: session.accessToken,
+    }
   });
 };
--- src/middlewares/validateBody.js
+++ src/middlewares/validateBody.js
@@ -3,6 +3,7 @@ import createHttpError from "http-errors";
 export const validateBody = (schema) => {
     return async (req, res, next) => {
         try {
+            console.log('test');
             await schema.validateAsync(req.body, {
                 abortEarly: false,
             });
